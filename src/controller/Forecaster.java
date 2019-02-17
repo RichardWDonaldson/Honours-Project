cf1cbcf432f380120b8b2a70125313ed421dc9f9
Richard Donaldson <PrincessNeckBeard@users.noreply.github.com>
2019-02-17 16:43:35 +0000

    Create Forecaster.java

---
+++ src/controller/Forecaster.java
@@ -0,0 +1,21 @@
+package controller;
+
+import weka.core.Instances;
+
+public class Forecaster {
+	
+	
+public void getForecast(Instances instance) {
+	
+//WekaForecaster forecaster = new WekaForecaster();	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+
+}

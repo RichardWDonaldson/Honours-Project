820af8cb49e94aca2d4560e46eca48a94f50bb86
Richard Donaldson <PrincessNeckBeard@users.noreply.github.com>
2019-02-17 18:20:53 +0000

    Update Forecaster.java

--- src/controller/Forecaster.java
+++ src/controller/Forecaster.java
@@ -1,15 +1,70 @@
 package controller;
 
+import java.io.File;
+import java.util.List;
 import weka.core.Instances;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.evaluation.NumericPrediction;
+import weka.classifiers.timeseries.WekaForecaster;
 
 public class Forecaster {
 	
 	
-public void getForecast(Instances instance) {
+public void getForecast(File arffFile, String fieldsToForecast, int classIndex) {
 	
-//WekaForecaster forecaster = new WekaForecaster();	
+
+
+
+
+	
+try {
+	//TODO instance is null
+	Instances instance = MLP.getInstances(arffFile.getName(), classIndex);	
 	
 	
+	WekaForecaster forecaster = new WekaForecaster();	
+	
+	
+	
+	forecaster.setFieldsToForecast(fieldsToForecast);
+	
+	forecaster.setBaseForecaster(new MultilayerPerceptron());
+	
+	forecaster.getTSLagMaker().setTimeStampField("Date");
+	forecaster.getTSLagMaker().setMinLag(1);
+	forecaster.getTSLagMaker().setMaxLag(52);
+	
+	
+	forecaster.getTSLagMaker().setAddDayOfWeek(true);
+	
+	
+	forecaster.buildForecaster(instance, System.out);
+	
+	forecaster.primeForecaster(instance);
+	
+	List<List<NumericPrediction>> forecast = forecaster.forecast(12, System.out);
+	
+	for(int i = 0; i < 12; i++) {
+		List<NumericPrediction> predsAtStep = forecast.get(i);
+		
+		for(int j = 0; j < 2; j++) {
+			NumericPrediction predForTarget = predsAtStep.get(j);
+			
+			System.out.print("" + predForTarget.predicted() + " ");
+		}
+		
+		System.out.println();
+	}
+	
+	
+	
+	
+	
+	
+} catch (Exception e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}	
 	
 	
 	

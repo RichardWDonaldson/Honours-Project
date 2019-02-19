deacdca6660c00363466cda4204cc7667911f73a
Richard Donaldson <PrincessNeckBeard@users.noreply.github.com>
2019-02-19 16:25:51 +0000

    Create Result.java

---
+++ src/model/Result.java
@@ -0,0 +1,75 @@
+package model;
+
+public class Result {
+	
+double correlationCoefficient;
+double meanAbsoluteError;
+double rootMeanSquredError;
+double relativeAbsoluteError;
+double rootRelativeSquredError;
+double instances;
+public Result(double correlationCoefficient, double meanAbsoluteError, double rootMeanSquredError,
+		double relativeAbsoluteError, double rootRelativeSquredError, double instances) {
+	super();
+	this.correlationCoefficient = correlationCoefficient;
+	this.meanAbsoluteError = meanAbsoluteError;
+	this.rootMeanSquredError = rootMeanSquredError;
+	this.relativeAbsoluteError = relativeAbsoluteError;
+	this.rootRelativeSquredError = rootRelativeSquredError;
+	this.instances = instances;
+}
+public double getCorrelationCoefficient() {
+	return correlationCoefficient;
+}
+public void setCorrelationCoefficient(double correlationCoefficient) {
+	this.correlationCoefficient = correlationCoefficient;
+}
+public double getMeanAbsoluteError() {
+	return meanAbsoluteError;
+}
+public void setMeanAbsoluteError(double meanAbsoluteError) {
+	this.meanAbsoluteError = meanAbsoluteError;
+}
+public double getRootMeanSquredError() {
+	return rootMeanSquredError;
+}
+public void setRootMeanSquredError(double rootMeanSquredError) {
+	this.rootMeanSquredError = rootMeanSquredError;
+}
+public double getRelativeAbsoluteError() {
+	return relativeAbsoluteError;
+}
+public void setRelativeAbsoluteError(double relativeAbsoluteError) {
+	this.relativeAbsoluteError = relativeAbsoluteError;
+}
+public double getRootRelativeSquredError() {
+	return rootRelativeSquredError;
+}
+public void setRootRelativeSquredError(double rootRelativeSquredError) {
+	this.rootRelativeSquredError = rootRelativeSquredError;
+}
+public double getInstances() {
+	return instances;
+}
+public void setInstances(double instances) {
+	this.instances = instances;
+}
+
+
+
+@Override
+public String toString() {
+	
+
+//TODO Fix toString so it outputs in the format I want	
+	return "Result [correlationCoefficient=" + correlationCoefficient + ", meanAbsoluteError=" + meanAbsoluteError
+			+ ", rootMeanSquredError=" + rootMeanSquredError + ", relativeAbsoluteError=" + relativeAbsoluteError
+			+ ", rootRelativeSquredError=" + rootRelativeSquredError + ", instances=" + instances + "]";
+
+	}
+
+
+	
+	
+
+}

9451182e2af603119195815b352b4faa51bec6b4
Richard Donaldson <PrincessNeckBeard@users.noreply.github.com>
2019-02-26 00:15:39 +0000

    Update LineChartView.java

--- src/view/LineChartView.java
+++ src/view/LineChartView.java
@@ -6,11 +6,20 @@ import java.util.List;
 
 import javax.swing.JFrame;
 
-
-
-
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
+
+
+
+
 
 
 public class LineChartView extends ApplicationFrame {

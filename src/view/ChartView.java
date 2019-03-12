436457760f754acb7785e5cf3750d90617b05aca
Richard Donaldson <PrincessNeckBeard@users.noreply.github.com>
2019-03-12 11:55:03 +0000

    Update ChartView.java

--- src/view/ChartView.java
+++ src/view/ChartView.java
@@ -2,6 +2,7 @@ package view;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -14,12 +15,13 @@ import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 
+import model.AdvancedSettings;
 import model.Result;
-
-
+import weka.core.Instances;
 
 public class ChartView extends ApplicationFrame {
-	
+	//TODO Split errors as two are values, two are percentages
+	//TODO graph correlation
 	public ChartView(String applicationTitle, String chartTitle, String categoryAxisLabel, String valueAxisLabel, List<Result> results) {
 		super(applicationTitle);
 		
@@ -27,7 +29,7 @@ public class ChartView extends ApplicationFrame {
 				chartTitle,
 				categoryAxisLabel, 
 				valueAxisLabel, 
-				createDataset(results),
+				standardDataset(results),
 				PlotOrientation.VERTICAL,
 				true, true, false);
 		
@@ -36,46 +38,67 @@ public class ChartView extends ApplicationFrame {
 		setContentPane(chartPanel);
 		
 	}
+	//constructor for evaluation
+	public ChartView(String applicationTitle, String chartTitle, String categoryAxisLabel, String valueAxisLabel, DefaultCategoryDataset dataset) {
+		super(applicationTitle);
+		
+		JFreeChart lineChart = ChartFactory.createLineChart(
+				chartTitle, 
+				categoryAxisLabel, 
+				valueAxisLabel, 
+				dataset,
+				PlotOrientation.VERTICAL,
+				true, true, false);
+		
+		ChartPanel chartPanel = new ChartPanel(lineChart);
+		chartPanel.setPreferredSize(new java.awt.Dimension( 560 , 367 ));
+		setContentPane(chartPanel);
+
+	}
+	
+	
 	
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	public void initialize(List<Result> results, ChartView window) {
+	public static void initialize(ChartView window) {
 
 		
 		window.pack( );
 	      RefineryUtilities.centerFrameOnScreen( window );
+	    window.setDefaultCloseOperation(window.DISPOSE_ON_CLOSE);
 	      window.setVisible( true );
-		
-		
-		
+			
 	}
 
-
-private DefaultCategoryDataset createDataset(List<Result> results) {
-	
-	
-	
-	
-	
-	int i = 1;
+public DefaultCategoryDataset standardDataset(List<Result> results) {
 	DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+	int i = 0;
+	
 	for(Result result : results) {
 		
 		dataset.addValue(result.getMeanAbsoluteError(), "Mean", Integer.toString(i));
 		dataset.addValue(result.getRelativeAbsoluteError(), "Relative", Integer.toString(i));
-		dataset.addValue(result.getRootMeanSquredError(), "Root Mean", Integer.toString(i));
-		dataset.addValue(result.getRootRelativeSquredError(), "Root Relative", Integer.toString(i));
+		dataset.addValue(result.getRootMeanSquaredError(), "Root Mean", Integer.toString(i));
+		dataset.addValue(result.getRootRelativeSquaredError(), "Root Relative", Integer.toString(i));
+		
+	
+		
 		
 	i++;
+	
 	}
 	
-
 	return dataset;
+	
 }
 
 
+
+
+
 }
+
 
 
 

f8451db9ec22b8bf3272872d4bea6068375575ad
Richard Donaldson <RichardWDonaldson@users.noreply.github.com>
2019-05-03 01:21:35 +0100

    Update RegressionSetting
    Update Regression Settings to inherit from Parent

--- src/model/RegressionSetting.java
+++ src/model/RegressionSetting.java
@@ -1,38 +1,51 @@
 package model;
 
-public class RegressionSetting {
-// -S 0: M5, 1: No attribute 2: Greedy Method     Attribute Selection
-	//-R Ridge 1.0E-8
-	//Decimal places -num-decimal-places 4
-	
-	
+public class RegressionSetting extends Settings {
+
+		
 	double ridge;
 	int decimalPlaces;
 	int attributeSelectionMethod;
-	public RegressionSetting(double ridge, int decimalPlaces, int attributeSelectionMethod) {
-		super();
+	
+	
+	public RegressionSetting(String algorithmType, double ridge, int decimalPlaces, int attributeSelectionMethod) {
+		super(algorithmType);
 		this.ridge = ridge;
 		this.decimalPlaces = decimalPlaces;
 		this.attributeSelectionMethod = attributeSelectionMethod;
 	}
+
+
 	public double getRidge() {
 		return ridge;
 	}
+
+
 	public void setRidge(double ridge) {
 		this.ridge = ridge;
 	}
+
+
 	public int getDecimalPlaces() {
 		return decimalPlaces;
 	}
+
+
 	public void setDecimalPlaces(int decimalPlaces) {
 		this.decimalPlaces = decimalPlaces;
 	}
+
+
 	public int getAttributeSelectionMethod() {
 		return attributeSelectionMethod;
 	}
+
+
 	public void setAttributeSelectionMethod(int attributeSelectionMethod) {
 		this.attributeSelectionMethod = attributeSelectionMethod;
 	}
+
+
 	@Override
 	public String toString() {
 		return "RegressionSetting [ridge=" + ridge + ", decimalPlaces=" + decimalPlaces + ", attributeSelectionMethod="
@@ -40,6 +53,11 @@ public class RegressionSetting {
 	}
 	
 	
+	
+	
+	
+	
+
 	
 	
 }
--- src/model/Settings.java
+++ src/model/Settings.java
@@ -1,5 +1,30 @@
 package model;
 
 public class Settings {
+	
+	String algorithmType;
+
+	public Settings(String algorithmType) {
+		super();
+		this.algorithmType = algorithmType;
+	}
+
+	public String getAlgorithmType() {
+		return algorithmType;
+	}
+
+	public void setAlgorithmType(String algorithmType) {
+		this.algorithmType = algorithmType;
+	}
+
+	@Override
+	public String toString() {
+		return "Settings [algorithmType=" + algorithmType + "]";
+	}
+	
+	
+	
+	
+	
 
 }
